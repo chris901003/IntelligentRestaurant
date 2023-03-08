4960033a857a8c21b94055b16cc413da53d7d744
chris901003 <a0987999103@gmail.com>
2023-03-08 19:19:14 +0800

    Initial Commit

---
+++ IntelligentRestaurant/IntelligentRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligentRestaurantApp.swift
+//  IntelligentRestaurant
+//
+//  Created by 黃弘諺 on 2023/3/8.
+//
+
+import SwiftUI
+
+@main
+struct IntelligentRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
